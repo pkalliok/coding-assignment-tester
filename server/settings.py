141d08013e25f6e7fc108191649f75b7d5be9e32
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2020-08-27 14:45:21 +0300

    Fix LANGUAGE_CODE to a working one

--- server/settings.py
+++ server/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'fi_FI'
+LANGUAGE_CODE = 'fi'
 
 TIME_ZONE = 'Europe/Helsinki'
 
