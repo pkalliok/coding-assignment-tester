7c364a8df6002fcf36bab7a291e0ac944b9348fc
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2020-08-30 13:16:18 +0300

    Test result report

--- tester/urls.py
+++ tester/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name='tester'
+
 urlpatterns = [
     path('', views.submission_form, name='solve-default'),
     path('<str:assignment>/solve', views.submission_form, name='solve'),
+    path('<str:assignment>/result', views.run_tests, name='runtests'),
 ]
--- tester/views.py
+++ tester/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.templatetags.static import static
+from django.urls import reverse
 
-def submission_form(request, assignment='fibonacci'):
+jinja_context = dict(
+        assignment='fibonacci',
+        url=reverse,
+        static=static,
+)
+
+def submission_form(request, **kw):
     return render(request, 'tester/submission_form.jinja',
-            {'assignment': assignment})
+            {**jinja_context, **kw})
+
+def run_tests(request, **kw):
+    return render(request, 'tester/result_report.jinja',
+            {**jinja_context, **kw})
 
