9a72d7a4d4c0ddf4d085cd95a41f6016d4e1ea5e
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2020-09-23 11:02:39 +0300

    munansaannos assignment

--- tester/testing.py
+++ tester/testing.py
@@ -1,6 +1,6 @@
 
 from requests import post
-from random import randint
+from random import randint, choice
 from json.decoder import JSONDecodeError
 
 class FailedTest(Exception):
@@ -43,9 +43,46 @@ def generate_flatten():
         inp = inp[:place] + [tag] + inp[place:]
         result = add_tag_at(result, place, tag)
 
+vowels = 'aeiouyåä'
+def random_vowel(): return choice(vowels)
+def random_consonant(): return choice('bcdðfghjkĸlmnpqrsštvwxz')
+
+def random_repeat(element):
+    if randint(0,1) == 0: return ''
+    return random_repeat(element) + element()
+
+def random_start():
+    return random_repeat(random_consonant) + \
+            random_vowel() + (random_vowel() if randint(0,2) == 0 else '')
+
+def random_non_start():
+    return random_consonant() + random_start()
+
+def random_end():
+    return random_repeat(random_non_start) + random_repeat(random_consonant)
+
+def random_swap_pair():
+    st1, st2 = random_start(), random_start()
+    end1, end2 = random_end(), random_end()
+    return ("%s%s %s%s" % (st1, end1, st2, end2),
+            "%s%s %s%s" % (st2, end1, st1, end2))
+
+def random_swapped_sentence():
+    if randint(0,3) == 0: return random_swap_pair()
+    if randint(0,2) == 0:
+        word = random_start() + random_end()
+        return (word, word)
+    s1, s2 = random_swapped_sentence()
+    w1, w2 = random_swap_pair()
+    return ('%s %s' % (w1, s1), '%s %s' % (w2, s2))
+
+def generate_swaps():
+    for i in range(30): yield random_swapped_sentence()
+
 test_gens = {
         'fibonacci': generate_fibo,
         'flatten': generate_flatten,
+        'munansaannos': generate_swaps,
 }
 
 def run_test_against(endpoint, inp, expected):
