5c1f0a8f23338332c9d93f6801fe90b34d6792d0
Panu Kalliokoski <panu.kalliokoski@sange.fi>
2021-01-11 16:29:14 +0200

    Tester and explanation for zerodepth

--- tester/testing.py
+++ tester/testing.py
@@ -80,10 +80,38 @@ def random_swapped_sentence():
 def generate_swaps():
     for i in range(30): yield random_swapped_sentence()
 
+def random_numbers(n):
+    return [randint(1,99) for _ in range(randint(0,n))]
+
+def extend_toplevel_zero(tree, mind, maxd):
+    return ([0] + tree + [0], 1, 1 if maxd is None else maxd)
+
+def extend_toplevel_nonzero(tree, mind, maxd):
+    return (random_numbers(3) + tree + random_numbers(3), mind, maxd)
+
+def embed_toplevel(tree, mind, maxd):
+    return ([tree],
+            None if mind is None else mind+1,
+            None if maxd is None else maxd+1)
+
+def embed_toplevel_imbalance(tree, mind, maxd):
+    return ([tree] + random_numbers(3) + [[tree]],
+            None if mind is None else mind+1,
+            None if maxd is None else maxd+2)
+
+def generate_zero_trees():
+    tree, mindepth, maxdepth = [], None, None
+    tree_trans = [extend_toplevel_zero, extend_toplevel_nonzero,
+            embed_toplevel, embed_toplevel_imbalance]
+    for i in range(30):
+        tree, mindepth, maxdepth = choice(tree_trans)(tree, mindepth, maxdepth)
+        yield (tree, [mindepth, maxdepth])
+
 test_gens = {
         'fibonacci': generate_fibo,
         'flatten': generate_flatten,
         'sananmuunnos': generate_swaps,
+        'zerodepth': generate_zero_trees,
 }
 
 def run_test_against(endpoint, inp, expected):
